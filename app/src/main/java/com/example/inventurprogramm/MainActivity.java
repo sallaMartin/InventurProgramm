380c57585c255d986927a17f2441c94ef6015e5c
Martin Sallaberger <martin-sallaberger@gmx.at>
2020-10-15 10:16:15 +0200

    Speichern Button

--- app/src/main/java/com/example/inventurprogramm/MainActivity.java
+++ app/src/main/java/com/example/inventurprogramm/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.inventurprogramm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText plainTextEan = (EditText) findViewById(R.id.plainTextEanView);
+        Button buttonSpeichern = (Button) findViewById(R.id.buttonSpeichern);
+
+        buttonSpeichern.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View arg0) {
+                plainTextEan.setText("Hello");
+            }
+        });
+
 
     }
 }
