1b1df502b71a0a8287cc020ef2359a44e8c5ea6a
Martin Sallaberger <martin-sallaberger@gmx.at>
2020-11-04 08:20:31 +0100

    layout in code

--- app/src/main/java/com/example/inventurprogramm/MainActivity.java
+++ app/src/main/java/com/example/inventurprogramm/MainActivity.java
@@ -7,12 +7,42 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    EditText plainTextEan;
+    TextView textViewEanNichtGefunden;
+
+    EditText plainTextMenge;
+    EditText plainTextLagerort;
+
+    Button buttonSpeichern;
+    TextView textViewStamm;
+    TextView textViewEingabe;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        plainTextEan = (EditText) findViewById(R.id.plainTextEanView);
+        textViewEanNichtGefunden = (TextView) findViewById(R.id.textViewEanNichtGefundenView);
+
+        plainTextMenge = (EditText) findViewById(R.id.plainTextMengeView);
+        plainTextLagerort = (EditText) findViewById(R.id.plainTextLagerortView);
+
+        buttonSpeichern = (Button) findViewById(R.id.buttonSpeichernView);
+        textViewStamm = (TextView) findViewById(R.id.textViewStammView);
+        textViewEingabe = (TextView) findViewById(R.id.textViewEingabeView);
+
+        buttonSpeichern.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+
     }
 }
