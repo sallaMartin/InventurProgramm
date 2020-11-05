c94eb8d051089f8c9a0d28792d65df057c0e082c
Martin Sallaberger <martin-sallaberger@gmx.at>
2020-11-05 09:56:29 +0100

    Layout changes 7.0 second activity

--- app/src/main/java/com/example/inventurprogramm/MainActivity.java
+++ app/src/main/java/com/example/inventurprogramm/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.inventurprogramm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -41,6 +42,8 @@ public class MainActivity extends AppCompatActivity {
         buttonSpeichern.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), MainActivity2.class);
+                startActivity(intent);
             }
         });
 
