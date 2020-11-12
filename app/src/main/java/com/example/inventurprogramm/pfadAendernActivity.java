15296d46b1f7920cc168f3e7c18bc47f346855c9
Jonathan Knecht <jonathanknecht@protonmail.com>
2020-11-12 09:48:34 +0100

    zurück Button hinzugefügt - erste Funktionalität

--- app/src/main/java/com/example/inventurprogramm/pfadAendernActivity.java
+++ app/src/main/java/com/example/inventurprogramm/pfadAendernActivity.java
@@ -1,8 +1,11 @@
 package com.example.inventurprogramm;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class pfadAendernActivity extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class pfadAendernActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pfad_aendern);
+
+        //back Button
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        //back Button
+        Intent intentMainActivity = new Intent(getBaseContext(), MainActivity.class);
+        startActivity(intentMainActivity);
+        return true;
+    }
+
+
 }
--- app/src/main/java/com/example/inventurprogramm/uebersichtActivity.java
+++ app/src/main/java/com/example/inventurprogramm/uebersichtActivity.java
@@ -1,8 +1,11 @@
 package com.example.inventurprogramm;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class uebersichtActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class uebersichtActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_uebersicht);
+
+        //back Button
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        //back Button
+        Intent intentMainActivity = new Intent(getBaseContext(), MainActivity.class);
+        startActivity(intentMainActivity);
+        return true;
     }
 }
